12bba52990d95cc39100eae0039bf2eecf4daec9
maijuriina <maiju.mustonen@gmail.com>
2019-10-31 10:55:17 +0200

    Calculator shows operation and calculates it with = button. You can then continue the operation from there. Pressing C empties the operation and screen.

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {clearElement} from '@angular/cdk/testing';
+import {$} from 'protractor';
 
 @Component({
   selector: 'app-calculator',
@@ -6,12 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-  
-  buttons: string[] = ["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "C", "0", "+", "="];
 
-  constructor() { }
+  buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '0', '+', '='];
+  calculation: string;
+
+  pressKey(inputValue: string) {
+    if (inputValue === 'C') {
+      this.calculation = '';
+    } else if (inputValue === '=') {
+      // tslint:disable-next-line:no-eval
+      this.calculation = eval(this.calculation);
+    } else {
+      this.calculation = this.calculation + inputValue;
+      console.log(this.calculation);
+    }
+  }
+
+  constructor() {
+    this.calculation = '';
+  }
 
   ngOnInit() {
   }
-
 }
