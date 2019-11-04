875856cd8417a102074567caf0cfdc2171292f72
maijuriina <maiju.mustonen@gmail.com>
2019-11-04 14:24:03 +0200

    Operand check now works.

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -10,7 +10,15 @@ import {$} from 'protractor';
 export class CalculatorComponent implements OnInit {
 
   buttons: string[] = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '0', '+', '='];
-  calculation: string;
+  calculation: any;
+  private value: any;
+
+  /*operandCheck(button) {
+    if (button === this.calculation.endsWith('-', this.calculation.length)) {
+      this.calculation = this.calculation.length - ;
+      console.log(this.calculation);
+    }
+  }*/
 
   pressKey(inputValue: string) {
     if (inputValue === 'C') {
@@ -18,9 +26,14 @@ export class CalculatorComponent implements OnInit {
     } else if (inputValue === '=') {
       // tslint:disable-next-line:no-eval
       this.calculation = eval(this.calculation);
+    } else if (inputValue === '/' || inputValue === '*' || inputValue === '+' || inputValue === '-') {
+      if (this.calculation.endsWith('/') || this.calculation.endsWith('*') ||
+        this.calculation.endsWith('+') || this.calculation.endsWith('-')) {
+      } else {
+        this.calculation += inputValue;
+      }
     } else {
-      this.calculation = this.calculation + inputValue;
-      console.log(this.calculation);
+      this.calculation += inputValue;
     }
   }
 
