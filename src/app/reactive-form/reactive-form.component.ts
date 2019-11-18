34ababf7a6b550aed1a3df0867a838f763b4a6c5
maijuriina <maiju.mustonen@gmail.com>
2019-11-18 14:59:30 +0200

    Reactive form now checks for checkbox, and carries information with onSubmit()-function after it has been checked.

---
+++ src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, AbstractControl, FormGroupDirective} from '@angular/forms';
+
+export class PasswordValidation {
+
+  static MatchPassword(AC: AbstractControl) {
+    let password = AC.get('password').value; // to get value in input tag
+    let confirmPassword = AC.get('confirmPwd').value; // to get value in input tag
+    console.log(password + ' ' + confirmPassword);
+    if (password !== confirmPassword) {
+      console.log('false');
+      AC.get('confirmPwd').setErrors( {MatchPassword: true} );
+    } else {
+      console.log('true');
+      return null;
+    }
+  }
+}
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent implements OnInit {
+
+  profileForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.profileForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[A-Öa-ö ]*')]],
+      lastName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('[A-Öa-ö ]*')]],
+      idNumber: ['', [Validators.required,
+        Validators.pattern('^(0[1-9]|[12]\\d|3[01])(0[1-9]|1[0-2])([5-9]\\d\\+|\\d\\d-|[01]\\dA)\\d{3}[\\dA-Z]$')]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$')]],
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.pattern('(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{10,}')]],
+        termsAndConditions: ['', Validators.required],
+      confirmPwd: ['', [Validators.required]]},
+    { validator: PasswordValidation.MatchPassword });
+    }
+
+  get firstName() {
+    return this.profileForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.profileForm.get('lastName');
+  }
+
+  get idNumber() {
+    return this.profileForm.get('idNumber');
+  }
+
+  get email() {
+    return this.profileForm.get('email');
+  }
+
+  get username() {
+    return this.profileForm.get('username');
+  }
+
+  get password() {
+    return this.profileForm.get('password');
+  }
+
+  get confirmPwd() {
+    return this.profileForm.get('confirmPwd');
+  }
+
+  onSubmit(reactForm: FormGroupDirective) {
+    console.log('Form sent!');
+    console.log(reactForm.value.firstName);
+  }
+}
